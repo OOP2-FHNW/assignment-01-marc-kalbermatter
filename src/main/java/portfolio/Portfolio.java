0148c7b52a15c6cf2e29c069fcdc9a25e4bc1e57
Marc Kalbermatter (s) <marc.kalbermatter@students.fhnw.ch>
2020-03-03 09:26:27 +0100

    tests successfull

--- src/main/java/portfolio/Portfolio.java
+++ src/main/java/portfolio/Portfolio.java
@@ -1,5 +1,56 @@
 package portfolio;
 
-public class Portfolio {
-    //TODO Complete this implementation and make it generic
+
+import portfolio.investments.Investment;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class Portfolio<T extends Investment> {
+
+  List<T> portfolio = new ArrayList<>();
+
+    public boolean contains(T s){
+        return portfolio.contains(s);
+    }
+
+    public void buy(T s){
+        for(int i = 0; i<portfolio.size();i++){
+            if(portfolio.get(i).equals(s)){
+                portfolio.get(i).setCount(portfolio.get(i).getCount()+s.getCount());
+            }
+
+        }
+        portfolio.add(s);
+    }
+
+    public void sell(String s, double num){
+        if(portfolio.contains(getShare(s))) {
+            getShare(s).setCount(getShare(s).getCount() - num);
+
+            if (getShare(s).getCount() < 1) {
+
+                portfolio.remove(getShare(s));
+            }
+        }
+
+
+    }
+
+    public double getCount(T s){
+        return s.getCount();
+    }
+
+    public T getShare(String s){
+        for(int i=0; i<portfolio.size(); i++){
+            if(portfolio.get(i).getTitel().equals(s)){
+                return portfolio.get(i);
+            }
+
+        }
+
+        return null;
+
+    }
 }
