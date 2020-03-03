683508b475b38acd56674b3f00b841b8fe6708ed
Marc Kalbermatter (s) <marc.kalbermatter@students.fhnw.ch>
2020-03-03 10:50:59 +0100

    some cleanup

--- src/main/java/portfolio/Portfolio.java
+++ src/main/java/portfolio/Portfolio.java
@@ -16,41 +16,34 @@ public class Portfolio<T extends Investment> {
     }
 
     public void buy(T s){
+
+        // Need to look if Object is already in Portfolio
         for(int i = 0; i<portfolio.size();i++){
             if(portfolio.get(i).equals(s)){
-                portfolio.get(i).setCount(portfolio.get(i).getCount()+s.getCount());
+                    portfolio.get(i).setCount(portfolio.get(i).getCount()+s.getCount()); // add newly bought shares to existing ones
+                return;
             }
-
         }
-        portfolio.add(s);
+        portfolio.add(s); // Shares weren't in portfolio
     }
+
 
     public void sell(String s, double num){
         if(portfolio.contains(getShare(s))) {
             getShare(s).setCount(getShare(s).getCount() - num);
-
             if (getShare(s).getCount() < 1) {
-
                 portfolio.remove(getShare(s));
             }
         }
-
-
     }
 
-    public double getCount(T s){
-        return s.getCount();
-    }
 
     public T getShare(String s){
         for(int i=0; i<portfolio.size(); i++){
             if(portfolio.get(i).getTitel().equals(s)){
                 return portfolio.get(i);
             }
-
         }
-
-        return null;
-
+       return null;
     }
 }
